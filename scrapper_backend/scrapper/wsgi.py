a0cfd8de30efe177a51e784a0a5172f32c6b1c0e
sebastien-mottet <sebastien.mottet96@gmail.com>
2022-03-25 12:10:26 +0100

    update config

--- scrapper_backend/scrapper/wsgi.py
+++ scrapper_backend/scrapper/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for insens project.
+WSGI config for immoweb_mail project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings.development')
 
 application = get_wsgi_application()
